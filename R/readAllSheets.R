2144ecb4dd71b822234f7baa6715c1d8bcb6dcd1
Gareth Maddock <9137502+gtm19@users.noreply.github.com>
2018-12-03 17:12:17 +0000

    Re-add ellipsis argument to readAllSheets.R function, and add line which names the elements of the list output

--- R/readAllSheets.R
+++ R/readAllSheets.R
@@ -1,6 +1,7 @@
 #' Read all sheets from an xlsx file into R as a list
 #'
-#' @param filename the path of an \code{xlsx} file
+#' @param filename the path of an \code{xlsx} file.
+#' @param ... additional arguments to be passed to \code{read_xlsx}.
 #'
 #' @return a list of data frames: one for each sheet in the source file
 #' @importFrom readxl excel_sheets read_xlsx
@@ -9,15 +10,17 @@
 #' @export
 #'
 
-readAllSheets <- function(filename) {
+readAllSheets <- function(filename, ...) {
 
   sheets <- readxl::excel_sheets(filename)
 
   list <-
     sheets %>%
     purrr::map(~{
-      readxl::read_xlsx(filename, .x)
+      if(length(list(...)) == 0) readxl::read_xlsx(filename) else readxl::read_xlsx(filename, .x)
     })
+
+  names(list) <- sheets
 
   return(list)
 }
