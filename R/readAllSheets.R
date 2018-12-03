d65b22183107ce15d49d85f167c9571cc3708d86
Gareth Maddock <9137502+gtm19@users.noreply.github.com>
2018-12-03 12:07:57 +0000

    Remove recursive part of readAllSheets function temporarily

--- R/readAllSheets.R
+++ R/readAllSheets.R
@@ -1,7 +1,6 @@
 #' Read all sheets from an xlsx file into R as a list
 #'
 #' @param filename the path of an \code{xlsx} file
-#' @param ... additional arguments passed to read_xlsx
 #'
 #' @return a list of data frames: one for each sheet in the source file
 #' @importFrom readxl excel_sheets read_xlsx
@@ -10,14 +9,14 @@
 #' @export
 #'
 
-readAllSheets <- function(filename, ...) {
+readAllSheets <- function(filename) {
 
   sheets <- readxl::excel_sheets(filename)
 
   list <-
     sheets %>%
     purrr::map(~{
-      readxl::read_xlsx(filename, .x, ...)
+      readxl::read_xlsx(filename, .x)
     })
 
   return(list)
