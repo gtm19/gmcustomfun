24d05fe37cce014c689404722da2d8d8e5b429a5
Gareth Maddock <9137502+gtm19@users.noreply.github.com>
2018-12-03 09:20:47 +0000

    Minor changes to the way dependencies have been declared

--- R/cleanColNames.R
+++ R/cleanColNames.R
@@ -6,18 +6,16 @@
 #'
 #' @return a (tidied) data frame
 #'
-#'
+#' @importFrom dplyr select_all
+#' @importFrom stringr str_to_lower str_replace_all str_remove_all
 #'
 #' @export
-#' @import dplyr stringr
 
 cleanColNames <- function(df) {
-  require(dplyr)
-  require(stringr)
 
   df %>%
-    select_all(str_to_lower) %>%  # make lower case
-    select_all(str_replace_all, "(-|—|–|:|;|\\(|\\)|\\{|\\}| )+", "_") %>% # replace multiple instances of common separators with single underscore
-    select_all(str_remove_all, "[^[a-z0-9_]]|_$")  # remove anything that's not lowercase letter, number, or underscore. Also remove trailing underscores.
+    dplyr::select_all(stringr::str_to_lower) %>%  # make lower case
+    dplyr::select_all(stringr::str_replace_all, "(-|—|–|:|;|\\(|\\)|\\{|\\}| )+", "_") %>% # replace multiple instances of common separators with single underscore
+    dplyr::select_all(stringr::str_remove_all, "[^[a-z0-9_]]|_$")  # remove anything that's not lowercase letter, number, or underscore. Also remove trailing underscores.
 
 }
