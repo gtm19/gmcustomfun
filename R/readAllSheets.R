3ddf5ef8a5323581f28d76eb390e144ee2949f99
Gareth Maddock <9137502+gtm19@users.noreply.github.com>
2018-12-28 09:36:50 +0000

    Changed compatibility of function to accept both xlsx and xls files

--- R/readAllSheets.R
+++ R/readAllSheets.R
@@ -1,10 +1,13 @@
-#' Read all sheets from an xlsx file into R as a list
+#' @title Read all sheets from an xlsx or xls file into R as a list
 #'
-#' @param filename the path of an \code{xlsx} file.
-#' @param ... additional arguments to be passed to \code{read_xlsx}.
+#' @description Simple function which reads all sheets from an \code{xlsx} or \code{xls} file into a list
+#'
+#' @param filename the path of an \code{xlsx} or \code{xls} file.
+#' @param ... additional arguments to be passed to \link[readxl]{read_xlsx}.
 #'
 #' @return a list of data frames: one for each sheet in the source file
-#' @importFrom readxl excel_sheets read_xlsx
+#' @importFrom readxl excel_sheets read_xlsx read_xls
+#' @importFrom stringr str_extract str_to_lower
 #' @importFrom purrr map
 #' @import magrittr
 #' @export
@@ -12,13 +15,34 @@
 
 readAllSheets <- function(filename, ...) {
 
+  variables <-
+    list(...)
+
+  extension <-
+    str_extract(filename, "(?<=\\.).{3,4}$") %>%
+    str_to_lower()
+
+  if (extension != "xls" & extension != "xlsx") {
+    stop("Filename must have either xlsx or xls extension")
+  }
+
   sheets <- readxl::excel_sheets(filename)
 
-  list <-
-    sheets %>%
-    purrr::map(~{
-      if(length(list(...)) == 0) readxl::read_xlsx(filename) else readxl::read_xlsx(filename, .x)
-    })
+  if (extension == "xlsx") {
+    list <-
+      sheets %>%
+      purrr::map(~{
+        if(length(variables) == 0) readxl::read_xlsx(filename, sheet = .x) else readxl::read_xlsx(filename, sheet = .x, ...)
+      })
+  }
+
+  if (extension == "xls") {
+    list <-
+      sheets %>%
+      purrr::map(~{
+        if(length(variables) == 0) readxl::read_xls(filename, sheet = .x) else readxl::read_xls(filename, sheet = .x, ...)
+      })
+  }
 
   names(list) <- sheets
 
